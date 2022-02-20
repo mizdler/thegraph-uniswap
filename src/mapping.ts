5530dabb02d8dd218848347c55a3be2801ab8fb4
Jiri <mohandes.jiri@gmail.com>
2022-02-20 18:38:58 +0330

    Initial commits

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,84 +1,56 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  UniswapV2Pair,
-  Approval,
   Burn,
   Mint,
   Swap,
   Sync,
-  Transfer
 } from "../generated/UniswapV2Pair/UniswapV2Pair"
-import { ExampleEntity } from "../generated/schema"
+import {
+  ReseveRecord,
+  BurnRecord,
+  MintRecord,
+  SwapRecord,
+} from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.MINIMUM_LIQUIDITY(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.burn(...)
-  // - contract.decimals(...)
-  // - contract.factory(...)
-  // - contract.getReserves(...)
-  // - contract.kLast(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.price0CumulativeLast(...)
-  // - contract.price1CumulativeLast(...)
-  // - contract.symbol(...)
-  // - contract.token0(...)
-  // - contract.token1(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+export function handleBurn(event: Burn): void {
+  const id = event.transaction.hash.toHexString()
+  const burnRecord = new BurnRecord(id)
+  burnRecord.timestamp = event.block.timestamp
+  burnRecord.sender = event.params.sender
+  burnRecord.amount0= event.params.amount0
+  burnRecord.amount1 = event.params.amount1
+  burnRecord.to = event.params.to
+  burnRecord.save()
 }
 
-export function handleBurn(event: Burn): void {}
+export function handleMint(event: Mint): void {
+  const id = event.transaction.hash.toHexString()
+  const mintRecord = new MintRecord(id)
+  mintRecord.timestamp = event.block.timestamp
+  mintRecord.sender = event.params.sender
+  mintRecord.amount0= event.params.amount0
+  mintRecord.amount1 = event.params.amount1
+  mintRecord.save()
+}
 
-export function handleMint(event: Mint): void {}
+export function handleSwap(event: Swap): void {
+  const id = event.transaction.hash.toHexString()
+  const swapRecord = new SwapRecord(id)
+  swapRecord.timestamp = event.block.timestamp
+  swapRecord.sender = event.params.sender
+  swapRecord.amount0In= event.params.amount0In
+  swapRecord.amount1In= event.params.amount1In
+  swapRecord.amount0Out= event.params.amount0Out
+  swapRecord.amount1Out= event.params.amount1Out
+  swapRecord.to = event.params.to
+  swapRecord.save()
+}
 
-export function handleSwap(event: Swap): void {}
-
-export function handleSync(event: Sync): void {}
-
-export function handleTransfer(event: Transfer): void {}
+export function handleSync(event: Sync): void {
+  const id = event.transaction.hash.toHexString()
+  const reserveRecord = new ReseveRecord(id)
+  reserveRecord.timestamp = event.block.timestamp
+  reserveRecord.reserve0 = event.params.reserve0
+  reserveRecord.reserve1 = event.params.reserve1
+  reserveRecord.save()
+}
